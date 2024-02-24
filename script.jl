88370e2425be2d605a7bcf4f002232053ab7ed0a
Alpha Olomi <alphaolomi@gmail.com>
2024-02-24 12:18:15 +0300

    Add new Julia script

---
+++ script.jl
@@ -0,0 +1,11 @@
+# Simple Julia script
+# Date: 2024-02-24
+# Author: Alpha Olomi
+
+# Main function
+function main()
+    println("Hello Julia, I am a script")
+end
+
+# Call the main function
+main()
